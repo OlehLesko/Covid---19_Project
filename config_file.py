b898196e44524e71ea4f08fb2154cd4bad2f5de1
roman <romanpridiba@gmail.com>
2021-11-13 17:30:57 +0200

    deleted unnecessary rows of code

--- config_file.py
+++ config_file.py
@@ -14,32 +14,7 @@ headers_ukraine = {'accept':"text/html,application/xhtml+xml,application/xml;q=0
 'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"}
 
 url_ukraine = 'https://index.minfin.com.ua/ua/reference/coronavirus/geography/asia/'
-#
-# def request(url):
-#     s = requests.Session()
-#     if url == url_ukraine:
-#         r = s.get(url=url, headers=headers_ukraine)
-#         check_status(r)
-#     else:
-#         r=s.get(url=url,headers=headers_asia)
-#         check_status(r)
-#
-# def check_status(r):
-#     if r.status_code == 200:
-#         data = r.text
-#         soup = BS(data,"lxml")
-#         if url == url_ukraine:
-#             pass
-#         else:
-#             pd_asia(soup)
-#     else:
-#         print('Error')
-# def parsing_data(html):
-#     all_infected = f' Всього захворіло: {html.find("td",class_="bg-total larger").text}\n'
-#     deaths = f'Померло: {html.find_all("td", class_="bg-total")[3].text}\n'
-#     recovered = f'Вилікувались: {html.find_all("td", class_="bg-total")[5].text}\n'
-#     now_ill = f'Продовжують хворіти: {html.find_all("td", class_="bg-total")[7].text}\n'
-#     print (all_infected, deaths, recovered, now_ill)
+
 
 
 

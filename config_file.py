ba54d8acc57ab34c1fa9c4516355bd5c3568f3d4
roman <romanpridiba@gmail.com>
2021-11-13 22:40:48 +0200

    makes some changes

--- config_file.py
+++ config_file.py
@@ -4,26 +4,24 @@ first_window_size = '600x400'
 first_window_title = 'Covid-19'
 first_window_background = 'Blue'
 
+
 # Asia
-headers_asia = {'accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"}
+headers_asia = {
+    'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,"
+              "application/signed-exchange;v=b3;q=0.9",
+    'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/95.0.4638.69 Safari/537.36"}
 
 url_asia = 'https://index.minfin.com.ua/ua/reference/coronavirus/geography/asia/'
+
+
 # Ukraine
-headers_ukraine = {'accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"}
+headers_ukraine = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
+              'application/signed-exchange;v=b3;q=0.9',
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/95.0.4638.69 Safari/537.36 '}
 
-url_ukraine = 'https://index.minfin.com.ua/ua/reference/coronavirus/geography/asia/'
-
-
-
-
-
-
-
-
-#
-# if __name__ == "__main__":
-#     request(url='https://index.minfin.com.ua/ua/reference/coronavirus/geography/asia/')
+url_ukraine = 'https://index.minfin.com.ua/ua/reference/coronavirus/ukraine/'
 
 

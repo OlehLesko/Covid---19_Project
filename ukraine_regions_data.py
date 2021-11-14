baa58b1eecc85e6b9047cee16b1fb677f9ca3257
roman <romanpridiba@gmail.com>
2021-11-14 08:21:33 +0200

    add some comments

--- ukraine_regions_data.py
+++ ukraine_regions_data.py
@@ -5,13 +5,13 @@ from general_info_ukr import general_data_ukr
 headers = config_file.headers_ukraine
 html = request(config_file.url_ukraine, headers)
 
-
+# get list of Ukraine region
 def get_region_list():
     region_of_ukraine = html.find("div", class_="compact-table expand-table").find("table").find_all("tr")
     for element in region_of_ukraine[1:-1]:
         print(element.find("a").text)
 
-
+# get data from each gerion of Ukraine
 def region_data_ukr():
     input_text = input("Enter text:")
     all_info = html.find_all('table')[1].find_all('td')

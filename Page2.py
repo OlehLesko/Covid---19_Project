e4dcb8211197f5911b9f825e95f08dfe46641c32
oleh <olehlesko81@gmail.com>
2021-11-21 00:19:07 +0200

    I added "Label_Phone_Number"

--- Page2.py
+++ Page2.py
@@ -9,12 +9,27 @@ def create_general_window():
     first_window.title(config.first_window_title)
     first_window['bg'] = config.first_window_background
 
+
+    #Labels
     Text_first_window = Label(first_window,
                               text='Covid-19',
                               bg='Blue', fg='White',
                               font=('Arial', 35),
                               width=10, height=1)
     Text_first_window.place(x=170, y=110)
+
+    Label_Phone_Number = Label(first_window,
+                               text = 'Гаряча лінія: '
+                                      '0 800 60 20 19',
+
+                               fg='Black', font=('Arial', 15),
+                               width=70, height=1)
+
+    Label_Phone_Number.place(x=0, y=0)
+
+
+
+    #Buttons
     Button_Europe_first_window = Button(first_window,
                                         text='Europe',
                                         bg='Black', fg='White',

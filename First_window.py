545f8e8748696d72720e47609c732d18650eba09
oleh <olehlesko81@gmail.com>
2021-11-22 14:39:24 +0200

    I changed the name Page

---
+++ First_window.py
@@ -0,0 +1,66 @@
+from tkinter import *
+from django.conf.locale import tk
+
+import config_file
+
+
+# Creating general window of program
+def create_general_window():
+    first_window = Tk()
+    first_window.iconbitmap('Coronavirus-300x300.ico')
+
+    canvas_width = 1005
+    canvas_height = 1000
+    canvas = Canvas(first_window,
+               width=canvas_width,
+               height=canvas_height)
+    canvas.pack()
+    first_window.geometry(config_file.first_window_size)
+    first_window.title(config_file.first_window_title)
+
+    photo = PhotoImage(file='covid19 title.png')
+    canvas.create_image(370, 200, image=photo)
+
+
+
+    canvas.create_text(470, 80, text="Covid-19", fill="White", font=('Arial', 40))
+    canvas.create_text(515, 180, text="Find out the information:", fill="White", font=('Arial', 20))
+
+    Label_Phone_Number = Label(first_window,
+                               text = 'Гаряча лінія: '
+                                      '0 800 60 20 19',
+
+                               fg='Black', font=('Arial', 15),
+                               width=70, height=1, anchor="w", justify="left")
+
+    Label_Phone_Number.place(x=0, y=0)
+
+
+
+    #Buttons
+    Button_Europe_first_window = Button(canvas,
+                                        text='Europe',
+                                        bg='White', fg='Black',
+                                        font=('Arial', 13),
+                                        width=11, height=2)
+    Button_Europe_first_window.place(x=370, y=240)
+    Button_Ukraine_first_window = Button(canvas,
+                                         text='Ukraine',
+                                         bg='White', fg='Black',
+                                         font=('Arial', 13),
+                                         width=11, height=2)
+    Button_Ukraine_first_window.place(x=500, y=240)
+
+    Button_Asia_first_window = Button(canvas,
+                                      text='Asia',
+                                      bg='White', fg='Black',
+                                      font=('Arial', 13),
+                                      width=11, height=2)
+    Button_Asia_first_window.place(x=630, y=240)
+
+
+    first_window.mainloop()
+
+
+if __name__ == "__main__":
+    create_general_window()

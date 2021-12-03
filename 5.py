1ef176b21fb98e8dd40e810a373fae775df4db91
admin <romanpridiba@gmail.com>
2021-12-03 11:06:16 +0200

    5.

--- 5.py
+++ 5.py
@@ -2,43 +2,28 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 root = Tk()
-root.geometry("800x500")
+w = root.winfo_screenwidth()
+h = root.winfo_screenheight()
 
-# Define image
-bg = ImageTk.PhotoImage(file="Images/image of main window.png")
+bg1 = Image.open("Images/1.png")
 
-# Create a canvas
+new_bg = ImageTk.PhotoImage(bg1)
 my_canvas = Canvas(root, width=800, height=500)
 my_canvas.pack(fill="both", expand=True)
 
-# Set image in canvas
-my_canvas.create_image(0,0, image=bg, anchor="nw")
-
-# Add a label
-my_canvas.create_text(400, 250, text="Welcome!", font=("Helvetica", 50), fill="white")
-
-# add some buttons
-button1 = Button(root, text="Start")
-button2 = Button(root, text="Reset Scores")
-button3 = Button(root, text="Exit")
-
-button1_window = my_canvas.create_window(10, 10, anchor="nw", window=button1)
-button2_window = my_canvas.create_window(100, 10, anchor="nw", window=button2)
-button3_window = my_canvas.create_window(230, 10, anchor="nw", window=button3)
-
 
 def resizer(r):
-    global bg1, resizer_bg, new_bg
-
-    bg1 = Image.open("Images/image of main window.png")
-
-    resized_bg = bg1.resize((r.width, r.height), Image.ANTIALIAS)
-
+    global bg1, new_bg
+    resized_bg = bg1.resize((r.width, r.height))
     new_bg = ImageTk.PhotoImage(resized_bg)
 
-    my_canvas.create_image(0,0, image=new_bg, anchor="nw")
+    my_canvas.create_image(0, 0, image=new_bg, anchor="nw")
+    my_canvas.create_text(300, 150, text="Covid-19", fill="White", font=('Arial', 80))
+    my_canvas.create_text(375, 320, text="Find out the information:", fill="White", font=('Arial', 40))
 
-    my_canvas.create_text(400, 250, text="Welcome!", font=("Helvetica", 50), fill="white")
+    my_canvas.create_text(1180, 25, text="Contact center of the MOZ:", fill="White", font=('Arial', 18))
+
+    my_canvas.create_text(1242, 53, text="0 800 60 20 19", fill="White", font=('Arial', 18))
 
 
 root.bind('<Configure>', resizer)

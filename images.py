c15f850ca54770d1b5da77623388df68d8d9ee52
IhorMar <rmg1203@gmail.com>
2021-12-06 13:41:06 +0200

    test

--- images.py
+++ images.py
@@ -8,7 +8,7 @@ class MyFrame2(wx.Frame):
                           size=wx.Size(1366, 700), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
         self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
         bSizer11 = wx.BoxSizer(wx.HORIZONTAL)
-        self.img1 = wx.Image("2image.png", wx.BITMAP_TYPE_ANY)
+        self.img1 = wx.Image("image of main window.png", wx.BITMAP_TYPE_ANY)
 
         self.m_bitmap3 = wx.StaticBitmap(self, wx.ID_ANY, wx.BitmapFromImage(self.img1), wx.DefaultPosition,
                                          wx.DefaultSize, 0)
@@ -17,7 +17,16 @@ class MyFrame2(wx.Frame):
         self.Bind(wx.EVT_SIZE, self.onResize)
         self.SetSizer(bSizer11)
         self.Layout()
-        self.Centre(wx.BOTH)
+        # self.Centre(wx.BOTH)
+
+        self.button1 = wx.Button(
+            self.m_bitmap3, label='Button1', size=(100, 100),
+            pos=(100, 100))
+
+        self.button1.Bind(wx.EVT_BUTTON, self.rdt)
+
+    def rdt(self, event):
+        print('Hello')
 
     def onResize(self, event):
         self.Layout()

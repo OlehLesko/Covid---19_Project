84f49a8404f9f22833d21eb710c4b54ccfe44f16
oleh <olehlesko81@gmail.com>
2021-12-10 16:23:10 +0200

    I did adaptive interface on Europe window

--- Europe_window.py
+++ Europe_window.py
@@ -44,26 +44,27 @@ def europe_function():
     Europe_window.state(first_window_size)
     Europe_window.title('Covid-19 information')
     Europe_window.iconbitmap('Images/icon.ico')
-    Europe_window.resizable(width=False, height=False)
+    Europe_window.minsize(600, 400)
 
     canvas = Canvas(Europe_window,
                     width=1920,
                     height=1080)
-    fon_image = im.open("Images/Image_of_window_Ukraine, Europe, Asia.png")
+    fon_image = im.open("Images/Virus_E_U_A.png")
     resized_image = fon_image.resize((2548, 1176))
     fon_photo = imtk.PhotoImage(resized_image)
 
     # transparent text_1
-    canvas.create_image(1200, 580, image=fon_photo)
-    canvas.create_text(300, 180, text="Select a country:", fill="White",
-                       font=('Arial', 53))
+    canvas.create_image(0, 0, anchor=NW,image=fon_photo)
 
+    txt_select_a_country = Label(canvas, text="Select a country:", bg="#d7d5d8", font=('Arial', 56))
+    txt_select_a_country.place(relx=0, rely=0.2)
     canvas.pack()
 
 
     def show_result(event):
         Field_Europe.delete(1.0, END)
         get_combobox_country = Combobox_Europe_second_window.get()
+
         if get_combobox_country in top_10:
             last_updated = sorted_dic[get_combobox_country.lower()]["last_updated"]
             confirmed = sorted_dic[get_combobox_country.lower()]["confirmed"]
@@ -79,7 +80,7 @@ def europe_function():
                                       f'Deaths: {deaths}\n \n'
                                       f'Total vaccinated: {total_vaccinated}\n'))
 
-            # Field_Europe['state'] = 'disabled'
+
             if '<<ComboboxSelected>>':
                 Field_Europe.delete(1.0, END)
                 get_combobox_second = Combobox_Europe_second_window.get()
@@ -108,13 +109,13 @@ def europe_function():
     Combobox_Europe_second_window.option_add('*TCombobox*Listbox.font', text_font)
     Combobox_Europe_second_window.set("Choose a country")
     Combobox_Europe_second_window['values'] = top_10
-    Combobox_Europe_second_window.place(x=60, y=250)
+    Combobox_Europe_second_window.place(relx=0.01, rely=0.35)
     Combobox_Europe_second_window.bind('<<ComboboxSelected>>', show_result)
 
 
     def return_to_first_window():
         Europe_window.destroy()
-        Start_App.create_general_window()
+        Main_window.create_general_window()
 
 
     return_to_main = Button(Europe_window, text='<<',
@@ -122,8 +123,37 @@ def europe_function():
     return_to_main.place(x=1, y=2)
 
     Field_Europe = Text(Europe_window, width=58, height=20, bg='White', fg='Black', font = ('Arial', 15))
-    Field_Europe.place(x=835, y=100)
+    Field_Europe.place(relx=0.52, rely=0.1)
 
+    def resize(e):
+        size = e.width
+
+        if size > 960 :
+            txt_select_a_country.config(font=('Arial', 36))
+            Field_Europe.config(width=58, height=20)
+            Field_Europe.place(relx=0.52, rely=0.1)
+
+
+
+
+
+        elif size > 790 :
+            txt_select_a_country.config(font=(None, 30))
+            Field_Europe.config(width=38, height=10)
+            Field_Europe.place(relx=0.01, rely=0.4)
+
+
+
+        else:
+            txt_select_a_country.config(font=('Arial', 28))
+            Field_Europe.config(width=37, height=12)
+            Field_Europe.place(relx=0.01, rely=0.45)
+
+
+
+
+
+    canvas.bind('<Configure>', resize)
     Europe_window.mainloop()
 
 if __name__ == "__main__":
